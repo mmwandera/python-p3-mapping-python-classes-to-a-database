b025436c62c5661fbea291948c98994b8398c57c
mmwandera <mmwandera74@gmail.com>
2024-02-07 12:19:35 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,12 @@
 from config import CONN, CURSOR
 from song import Song
 
+import pytest; pytest.set_trace()
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+# from config import CONN, CURSOR
+# from song import Song
+
+
+# if __name__ == '__main__':
+#     import ipdb; ipdb.set_trace()
+
